28c9a46deb9ea0de710015c026b1b836842e24b1
june17thcom <flwns002@gmail.com>
2025-04-24 17:50:57 +0900

    충돌 해결 후 stash pop 적용

---
+++ controller/users.mjs
@@ -0,0 +1,57 @@
+import e from "express";
+import * as authRepository from "../data/auth.mjs";
+
+//로그인
+//모든 포스트 / 해당 아이디에 대한 포스트를 가져오는 함수
+export async function login(req, res, next) {
+  const { userid, password } = req.body;
+  const user = await authRepository.login(userid, password);
+  if (user) {
+    req.session.user = { userid };
+    res.status(200).json(`${userid}님 로그인 완료`);
+  } else {
+    res.status(400).json(`${userid}님 로그인이 실패되셨습니다.`);
+  }
+}
+
+// 로그인 유지: 받아온 정보가 일치할 시에만
+//세션이 정상으로 유지되는 한
+export async function userCheck(req, res, next) {
+  const data = await req.session.user;
+  if (data) {
+    res.status(200).json(req.session.user);
+  } else {
+    res.status(401).send("로그인이 필요합니다.");
+  }
+}
+
+// 로그아웃
+// 세션에 저장된 유저 정보를 삭제하는 함수
+export async function logout(req, res, next) {
+  const user = req.session.user;
+  if (user) {
+    req.session.destroy(() => {
+      res.sendStatus(205);
+    });
+  } else {
+    res.status(404).json({
+      message: `현재 로그인 돼 있지 않습니다.`,
+    });
+  }
+}
+
+// 회원가입
+// 유저를 생성하는 함수
+export async function signUp(req, res, next) {
+  const { userid, name, password, email } = req.body;
+  const users = await authRepository.signUp(userid, name, password, email);
+  if (users) {
+    res.status(201).json(users);
+  } else if (users === false) {
+    res.status(409).json({ message: `${userid}는 이미 가입된 아이디입니다.` });
+  } else {
+    res
+      .status(404)
+      .json({ message: `${userid}의 회원을 생성하는 데 실패했습니다.` });
+  }
+}
--- router/auth.mjs
+++ router/auth.mjs
@@ -1,6 +1,10 @@
 import express from "express";
+<<<<<<< Updated upstream
 
 //const app = express();
+=======
+import * as authController from "../controller/users.mjs";
+>>>>>>> Stashed changes
 
 const router = express.Router();
 
