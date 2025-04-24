a4c226944711e929aaca5a35e69cdf548a50342d
june17thcom <flwns002@gmail.com>
2025-04-24 17:53:40 +0900

    Update auth.mjs

--- router/auth.mjs
+++ router/auth.mjs
@@ -1,11 +1,25 @@
 import express from "express";
-<<<<<<< Updated upstream
-
-//const app = express();
-=======
 import * as authController from "../controller/users.mjs";
->>>>>>> Stashed changes
+//const app = express();
 
 const router = express.Router();
+
+// 회원가입
+// POST
+// http://127.0.0.1:8080/auth/signup
+router.post("/signup", authController.signUp);
+
+// 로그인
+// POST
+// http://127.0.0.1:8080/auth/login
+router.post("/login", authController.login);
+
+// 로그인 유지
+// http://127.0.0.1:8080/auth/me
+router.get("/me", authController.userCheck);
+
+//로그아웃
+// http://127.0.0.1:8080/auth/logout
+router.get("/logout", authController.logout);
 
 export default router;
